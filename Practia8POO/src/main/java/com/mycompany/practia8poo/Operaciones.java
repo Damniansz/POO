f7312056b1ef4364bf21e51c59ccfb72f33bdfd9
Damniansz <adsaula@espe.edu.ec>
2023-05-15 09:07:57 -0500

    PracticaLab8

---
+++ Practia8POO/src/main/java/com/mycompany/practia8poo/Operaciones.java
@@ -0,0 +1,59 @@
+package com.mycompany.practia8poo;
+
+
+import javax.swing.JOptionPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ESPE
+ */
+public class Operaciones {
+    int num1;
+    int num2;
+    int suma;
+    int resta;
+    int multi;
+    int div;
+    //Generamos Metodos
+    //Pedimos Numeros
+    //Nuestro Metodo es publico
+    
+   
+    public void capturarnumeros (){
+        //Pedir numeros en pantalla
+        num1 = Integer.parseInt (JOptionPane.showInputDialog("Ingresar primer numero"));
+        num2 = Integer.parseInt (JOptionPane.showInputDialog("Ingresar primer numero"));
+
+    }
+    
+    public void suma (){
+        suma=num1+num2;
+    }
+    
+    public void resta (){
+        resta=num1-num2;
+    }
+    
+    public void multi (){
+        multi=num1*num2;
+    }
+    
+    public void div (){
+        div=num1/num2;
+    }
+    
+    public void resultados (){
+        System.out.println("El resultado de la suma es "+suma);
+        System.out.println("El resultado de la resta es "+resta);
+        System.out.println("El resultado de la multiplicacion es "+multi);
+        System.out.println("El resultado de la division es "+div);
+    }
+            
+    
+    
+}
---
+++ Practica8POO_Metodos_Parametros/src/main/java/com/mycompany/practica8poo_metodos_parametros/Practica8POO_Metodos_Parametros.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.practica8poo_metodos_parametros;
+
+/**
+ *
+ * @author ESPE
+ */
+public class Practica8POO_Metodos_Parametros {
+
+    public static void main(String[] args) {
+        Operaciones op1 = new Operaciones ();
+        
+        int nu1 = Integer.parseInt(JOptionPane.showInputDialog("Ingresar numero"));
+        int nu2 = Integer.parseInt(JOptionPane.showInputDialog("Ingresar numero"));
+        
+        op1.
+    }
+}
