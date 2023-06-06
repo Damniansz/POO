f0e7c41258df53c897e07ad551900f96cef082ac
Damniansz <132872306+Damniansz@users.noreply.github.com>
2023-06-05 23:49:25 -0500

    v1.0

---
+++ Cronograma/src/cronograma/Cronograma.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cronograma;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author xaldo
+ */
+public class Cronograma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int opc = 0;
+        int elegir = 0;
+
+        do {
+            System.out.println("Ingrese una opción:");
+            System.out.println("1. Ingresar Equipos");
+            System.out.println("2. Ingresar Participantes");
+            System.out.println("3. Ingresar Resultados");
+            System.out.println("4. Salir");
+
+            opc = sc.nextInt();
+
+            switch (opc) {
+                case 1:
+                    System.out.println("Ingresar Equipos");
+                    
+                    break;
+
+                case 2:
+                    System.out.println("Ingresar Participantes");
+                    
+                    break;
+
+                case 3:
+                    System.out.println("Ingresar Resultados");
+                    
+                    break;
+
+                case 4:
+                    System.out.println("Saliendo del programa");
+                    break;
+
+                default:
+                    System.out.println("Opción inválida. Intente nuevamente.");
+                    break;
+            }
+        } while (opc != 4);
+        sc.close();
+    }
+    
+}
