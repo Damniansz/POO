c3a1e6c89b942570cfa69a9baafb35d123a6d28d
Damniansz <132872306+Damniansz@users.noreply.github.com>
2023-06-09 23:56:09 -0500

    09/06/2023

---
+++ POO_Practica5_SaulaAldo/src/poo_practica5_saulaaldo/POO_Practica5_SaulaAldo.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poo_practica5_saulaaldo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author xaldo
+ */
+public class POO_Practica5_SaulaAldo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ArrayList<Persona> persona = new ArrayList<Persona>();
+        persona.add(new Persona("Juan","1111","juansz@gmail.com"));
+        persona.add(new Persona("Aldo","2222","adsz@gmail.com"));
+        persona.add(new Persona("Joseph","3333","mate@gmail.com"));
+        persona.add(new Persona("Maria","4444","Maria@gmail.com"));
+        
+         
+    }
+        
+    }
+    
+}
