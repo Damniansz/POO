7c852fb4ae161596e03393e82015c606aafead6a
Damniansz <132872306+Damniansz@users.noreply.github.com>
2023-08-17 01:09:56 -0500

    16/08/2023

---
+++ POO_GRAFICAS_SAULA/src/poo_graficas_saula/POO_GRAFICAS_SAULA.java
@@ -0,0 +1,16 @@
+
+package poo_graficas_saula;
+
+import Vista.Registro;
+
+
+public class POO_GRAFICAS_SAULA {
+
+    
+    public static void main(String[] args) {
+        Registro registro = new Registro();
+        registro.setVisible(true);
+        
+    }
+    
+}
